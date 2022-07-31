08cb68059f2be715f56265e5b6c6e76af723359c
Felix Dietze <felix.dietze@rwth-aachen.de>
2022-07-31 20:07:32 +0200

    add missing demo files

---
+++ demo/src/main/scala/Main.scala
@@ -0,0 +1,31 @@
+package webapp
+
+import outwatch._
+import outwatch.dsl._
+import cats.effect.SyncIO
+
+import colibri.reactive._
+
+import formidable._
+import formidable.instances._
+import formidable.Form._
+
+case class Person(name: String, age: Option[Int], pets: Seq[Pet])
+case class Pet(name: String, weight: Double)
+
+object Main {
+
+  def main(args: Array[String]): Unit =
+    Outwatch.renderInto[SyncIO]("#app", div(app)).unsafeRunSync()
+
+  def app: VModifier = Owned {
+
+    val state: Var[Person] = Form.state[Person]
+
+    div(
+      Form[Person].apply(state),
+      div(state.map(_.toString)),
+    ): VModifier
+  }
+
+}
